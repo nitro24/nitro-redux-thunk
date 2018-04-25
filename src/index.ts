078e7da939950c454f65fbf1fdf5d93013d7cd0f
Nitro <info@nitroh.io>
2018-04-24 21:02:20 -0400

    0.3.0
    Still testing out some typings

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import { Middleware, MiddlewareAPI, Dispatch, AnyAction, Action } from 'redux';
 
-export type ThunkAction<R, S extends Action = AnyAction> = (dispatch: Dispatch<S>, getState: () => S) => R;
+export interface ThunkDispatch<S = any, A extends Action = AnyAction> {
+  <T extends A>(action: T): T;
+  <R, S>(asyncAction: ThunkAction<R, S, A>): R;
+}
 
-const thunk: Middleware = <S, D extends Dispatch = Dispatch>(store: MiddlewareAPI<D, S>) =>
-  (next: Dispatch<AnyAction>) => (action: any): any => {
-    if (typeof action === 'function') {
-      return action(store.dispatch, store.getState);
-    }
-    return next(action);
-  };
+export type ThunkAction<R = {}, S = any, A extends Action = AnyAction> = (
+  dispatch: ThunkDispatch<S, A>,
+  getState: () => S,
+) => R;
+
+export type ThunkMiddleware<S = any, A extends Action = AnyAction> =
+  Middleware<ThunkDispatch<S, A>, S, ThunkDispatch<S, A>>;
+
+const thunk: ThunkMiddleware = <S = any, A extends Action = AnyAction>(store: MiddlewareAPI<ThunkDispatch<S, A>, S>) =>
+(next: Dispatch<AnyAction>) => (action: any): any => {
+  if (typeof action === 'function') {
+    return action(store.dispatch, store.getState);
+  }
+  return next(action);
+};
 
 export default thunk;
